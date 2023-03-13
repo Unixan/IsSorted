dea85ca5bf71fac585ebec5da9c3f3ecb87c83e7
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-13 13:48:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IsSorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] Array2 = new[] { 4, 2, 3, 1, 5, 10, 7, 8, 9, 6 };
+
+            bool value1 = IsSorted(Array1);
+            bool value2 = IsSorted(Array2);
+
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+        }
+
+        static bool IsSorted(int[] array)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - 1]) return false;
+            }
+            return true;
+        }
+    }
+}
